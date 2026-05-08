aa746f0c28766f1cf07702a9ca9c3940e17bb5a2
Davi Amaral <imyuno2022@outlook.com>
2026-05-07 22:46:07 -0300

    Aula 65

---
+++ Seção_ArraysAvançados/aula65/filter.js
@@ -0,0 +1,23 @@
+// Filter, map, reduce
+
+// Retorne os números maiores que 10
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+function callbackFilter(valor, indice, array){
+    
+}
+const numerosFiltrados = numeros.filter();
+
+
+// Retorne as pessoas que tem o nome, com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com A
+
+const pessoas = [ 
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55},
+    { nome: 'Letícia', idade: 19},
+    { nome: 'Rosana', idade: 32},
+    { nome: 'Wallace', idade: 47},
+]
